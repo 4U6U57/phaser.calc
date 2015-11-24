fae86de7a88027d8c0068b9390d6cd94739aff33
August Valera <4U6U57@gmail.com>
2015-11-24 13:50:04 -0800

    Fixed output to be easier to see

--- phasers.sh
+++ phasers.sh
@@ -12,19 +12,19 @@ while read -r line; do
       echo "Rect -> Polar"
       real=$2
       imag=$3
-      echo "Input: $real $imag"
+      echo "Input: $real + j$imag"
       rad=$(echo "scale=3; sqrt(($real^2)+($imag^2))" | bc -l)
       theta=$(echo "scale=3; (a($imag/$real)/$pi)*180" | bc -l)
-      echo "Output: $rad $theta"
+      echo "Output: $rad ∠ $theta"
       ;;
     pr | r)
       echo "Polar -> Rect"
       rad=$2
       theta=$3
-      echo "Input: $rad, $theta"
+      echo "Input: $rad ∠ $theta"
       real=$(echo "scale=3; $rad*(c(($theta*$pi)/180))" | bc -l)
       imag=$(echo "scale=3; $rad*(s(($theta*$pi)/180))" | bc -l)
-      echo "Output: $real $imag"
+      echo "Output: $real + j$imag"
       ;;
     pmult)
       echo "Polar * Polar"
@@ -32,11 +32,10 @@ while read -r line; do
       theta1=$3
       rad2=$4
       theta2=$5
-      echo "Input1: $rad1 $theta1"
-      echo "Input2: $rad2 $theta2"
+      echo "Input1: $rad1 ∠ $theta1 * $rad2 ∠ $theta2"
       rad=$(echo "scale=3; $rad1*$rad2" | bc -l)
       theta=$(echo "scale=3; $theta1+($theta2)" | bc -l)
-      echo "Output: $rad $theta"
+      echo "Output: $rad ∠ $theta"
       ;;
     pdiv)
       echo "Polar / Polar"
@@ -44,11 +43,10 @@ while read -r line; do
       theta1=$3
       rad2=$4
       theta2=$5
-      echo "Input1: $rad1 $theta1"
-      echo "Input2: $rad2 $theta2"
+      echo "Input: $rad1 ∠ $theta1 / $rad2 ∠ $theta2"
       rad=$(echo "scale=3; $rad1/$rad2" | bc -l)
       theta=$(echo "scale=3; $theta1-($theta2)" | bc -l)
-      echo "Output: $rad $theta"
+      echo "Output: $rad ∠ $theta"
       ;;
     rplus)
       echo "Rect + Rect"
@@ -56,11 +54,10 @@ while read -r line; do
       imag1=$3
       real2=$4
       imag2=$5
-      echo "Input1: $real1 $imag1"
-      echo "Input2: $real2 $imag2"
+      echo "Input1: $real1 + j$imag1 + $real2 + j$imag2"
       real=$(echo "scale=3; $real1+$real2" | bc -l)
       imag=$(echo "scale=3; $imag1+$imag2" | bc -l)
-      echo "Output: $real $imag"
+      echo "Output: $real + j$imag"
       ;;
     rminus)
       echo "Rect - Rect"
@@ -68,11 +65,10 @@ while read -r line; do
       imag1=$3
       real2=$4
       imag2=$5
-      echo "Input1: $real1 $imag1"
-      echo "Input2: $real2 $imag2"
+      echo "Input1: $real1 + j$imag1 - $real2 - j$imag2"
       real=$(echo "scale=3; $real1-$real2" | bc -l)
       imag=$(echo "scale=3; $imag1-$imag2" | bc -l)
-      echo "Output: $real $imag"
+      echo "Output: $real + j$imag"
       ;;
     edit)
       vi $0
